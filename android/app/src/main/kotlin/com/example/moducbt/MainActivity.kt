f0c90750ab2965a41894b6cb7cd799f97997708d
eungwang1 <scm323@naver.com>
2023-02-01 17:28:58 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/example/moducbt/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.moducbt
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:moducbt/features/main/main_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'moducbt',
+      theme: ThemeData(),
+      home: const MainScreen(),
+    );
+  }
+}
